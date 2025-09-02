7b0974b1ec677b1fbeb397a627b2f33d68023a8a
woo <5starshin@gmail.com>
2025-09-02 11:20:48 +0900

    과제 제출

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,31 @@
 def add(a, b):
-    pass
+    return a + b
 
 
 def sub(a, b):
-    pass
+    return a - b
 
 
 def mul(a, b):
-    pass
+    return a * b
 
 
 def div(a, b):
-    pass
+    return a / b
 
 
 def power(base, pow):
-    pass
+    return base ** pow
 
 
 def square(base):
-    pass
+    return base * base
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    if 나이 > 20:
+        return f"안녕하십니까 {이름}!"
+    elif 나이 == 20:
+        return f"안녕하신가 {이름}!"
+    else:
+        return f"안녕 {이름}!"
